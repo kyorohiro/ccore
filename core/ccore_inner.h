108baf0d06481b0af6d4b1aeff2ab46cb4397d53
kyorohiro <kyorohiro@kyorohironoMac-mini.local>
2017-09-20 17:53:45 +0900

    mod objectName

--- core/ccore_inner.h
+++ core/ccore_inner.h
@@ -1,23 +1,17 @@
 #ifndef _H_ccore_inner_
 #define _H_ccore_inner_
 
-#define CArrayList_NAME "CArrayList"
-#define CBytes_NAME "CBytes"
-#define CBytesBuilder_NAME "CBytesBuilder"
-
-
-#define CEventObserver_NAME "CEventObserver"
-#define CEventDispatcher_NAME "CEventDispatcher"
-#define CException_NAME "CException"
-#define CHashMap_NAME "CHashMap"
-#define CHashMapItem_NAME "CHashMapItem"
-#define CLinkedList_NAME "CLinkedList"
-
-
-#define CObject_NAME "obj"
-
-#define CObjectManager_NAME "CObjectManager"
-
-#define CString_NAME "str"
+#define CArrayList_NAME "ArraList"
+#define CBytes_NAME "Bytes"
+#define CBytesBuilder_NAME "BytesBui"
+#define CEventObserver_NAME "EventObs"
+#define CEventDispatcher_NAME "EventDis"
+#define CException_NAME "Except"
+#define CHashMap_NAME "HashMap"
+#define CHashMapItem_NAME "HashMapI"
+#define CLinkedList_NAME "LinkList"
+#define CObject_NAME "Object"
+#define CObjectManager_NAME "ObjMgr"
+#define CString_NAME "String"
 
 #endif
